73fb4c7d33ec953dd1caf4270b5595e043482b07
fdci-jeff <fdci.jd@gmail.com>
2023-02-27 17:45:51 +0800

    remove action bar on login screen

--- app/src/main/java/com/example/travelapp/LoginActivity.java
+++ app/src/main/java/com/example/travelapp/LoginActivity.java
@@ -1,14 +1,19 @@
 package com.example.travelapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.login);
     }
 }
