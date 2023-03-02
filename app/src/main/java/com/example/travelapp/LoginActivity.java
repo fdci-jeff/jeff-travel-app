47b2b41d408a88b994efa29061d7768951ca890b
fdci-jeff <fdci.jd@gmail.com>
2023-03-02 11:08:32 +0800

    remove all login

--- app/src/main/java/com/example/travelapp/LoginActivity.java
+++ app/src/main/java/com/example/travelapp/LoginActivity.java
@@ -3,41 +3,12 @@ package com.example.travelapp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Window;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.example.travelapp.model.User;
-import com.example.travelapp.network.ApiClient;
-import com.example.travelapp.network.ApiInterface;
-import com.example.travelapp.network.response.UserResponse;
-import com.example.travelapp.utils.SharedPrefManager;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
-
-    @BindView(R.id.username)
-    EditText username;
-    @BindView(R.id.password)
-    EditText password;
-
-    Context mContext;
-    ApiInterface apiInterface;
-    SharedPrefManager sharedPrefManager;
-    ProgressDialog progressDialog;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -45,50 +16,6 @@ public class LoginActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.login);
-        mContext = this;
 
-        ButterKnife.bind(this);
-        apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        sharedPrefManager = new SharedPrefManager(this);
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading");
-        progressDialog.setCancelable(false);
-
-
-        if (sharedPrefManager.getLogin()) {
-            startActivity(new Intent(LoginActivity.this, MainActivity.class)
-                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
-            finish();
-        }
-    }
-
-    @OnClick(R.id.btn_login) void login() {
-        progressDialog.show();
-        Call<UserResponse> postLogin = apiInterface.postLogin(username.getText().toString(), password.getText().toString());
-
-        postLogin.enqueue(new Callback<UserResponse>() {
-            @Override
-            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
-                progressDialog.dismiss();
-
-                if (response.code() == 200) {
-                    User user = response.body().getUser();
-                    Log.d("MyApp", response.body().getUser().toString());
-                    sharedPrefManager.saveSPString(SharedPrefManager.NAME, user.getName());
-                    sharedPrefManager.saveSPString(SharedPrefManager.TOKEN, "Bearer " + response.body().getToken());
-                    sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_LOGIN, true);
-                    startActivity(new Intent(mContext, MainActivity.class)
-                            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
-                    finish();;
-                } else {
-                    Toast.makeText(mContext, "Email/Password test", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<UserResponse> call, Throwable t) {
-                progressDialog.dismiss();
-            }
-        });
     }
 }
