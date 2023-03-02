51d537d2497d4411759c9ece861e3faa04e1127b
fdci-jeff <fdci.jd@gmail.com>
2023-03-02 17:50:47 +0800

    fix login

--- app/src/main/java/com/example/travelapp/LoginActivity.java
+++ app/src/main/java/com/example/travelapp/LoginActivity.java
@@ -1,14 +1,37 @@
 package com.example.travelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText usernameText;
+    EditText passwordText;
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -17,5 +40,59 @@ public class LoginActivity extends AppCompatActivity {
         actionBar.hide();
         setContentView(R.layout.login);
 
+        usernameText = findViewById(R.id.username);
+        passwordText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.btn_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
+
+
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        String requestBody = "{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}";
+        RequestBody body = RequestBody.create(requestBody, mediaType);
+
+        Request request = new Request.Builder()
+                .url("http://10.0.2.2:8000/api/auth/login")
+                .post(body)
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String token = response.body().string();
+
+                    SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("token", token);
+                    editor.apply();
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } else {
+                    // Handle login failure
+                    String error = response.body().string();
+                    Log.e("LoginActivity", "Login error: " + error);
+                }
+            }
+        });
     }
 }
